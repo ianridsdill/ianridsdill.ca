b34bf1a4aae7947c6f35629165db89776d43023b
Ian <jeezus84@gmail.com>
2018-04-08 11:28:14 -0400

    - updating link to gitlab. - adding angular material.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'ianridsdill.ca';
 }
